14e6370ae33060d5a480e919db3487fbd0648f18
Chanwoongs <bck4044@gmail.com>
2023-11-28 23:37:49 +0900

    object slicing

---
+++ C++/OOP/object_slicing.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+class Animal
+{
+	Animal& operator=(Animal other) = delete;
+	virtual void speak()
+	{
+		cout << "ani\n";
+	}
+	virtual ~Animal() = default;
+protected:
+	Animal(const Animal& other) = default;
+private:
+	double data = 0.0f;
+};
+
+bool operator==(const Animal& lhs, const Animal& rhs)
+{
+	return lhs.data == rhs.data;
+}
+
+class Cat : public Animal
+{
+public:
+	Cat(double d) : catData{ d } {}
+	virtual void speak() override
+	{
+		cout << "cat\n";
+	}
+private:
+	double catData;
+};
+
+void f(Aniaml animal) // reference 로 받아야한다.
+{
+
+}
+
+int main()
+{
+	Cat kitty{ 1.0 };
+	kitty.speak();
+
+	Animal& animalRef = kitty;
+	animalRef.speak(); // meow
+
+	Animal animalObj = kitty;
+	animalObj.speak(); // animal
+	return 0;
+}
