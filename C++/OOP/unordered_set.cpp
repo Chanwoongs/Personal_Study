d370fa448d16e32e7afa41df2570842afe1db6ac
Chanwoongs <bck4044@gmail.com>
2023-12-08 18:30:55 +0900

    unordered_set & class

--- C++/OOP/unordered_set.cpp
+++ C++/OOP/unordered_set.cpp
@@ -4,6 +4,57 @@
 
 using namespace std;
 
+class Cat
+{
+public:
+	Cat(int age, string name) : mAge{ age }, mName{ name } {}
+
+	const int age() const
+	{
+		return mAge;
+	}
+
+	const string name() const
+	{
+		return mName;
+	}
+
+private:
+	int mAge;
+	string mName;
+};
+
+// custom hash
+struct CatHash
+{
+	std::size_t operator()(Cat const& c) const noexcept
+	{
+		std::size_t h1 = std::hash<int>{}(c.age());
+		std::size_t h2 = std::hash<string>{}(c.name());
+		return h1 ^ h2;
+	}
+};
+
+// == operator overload
+bool operator== (const Cat& lhs, const Cat& rhs) noexcept
+{
+	return (lhs.age() == rhs.age() && lhs.name() == rhs.name());
+}
+
+// Custom specialization of std::hash can be injected in namespace std.
+namespace std
+{
+	template<>
+	struct std::hash<Cat>
+	{
+		std::size_t operator()(const Cat& c) const noexcept
+		{
+			std::size_t h1 = std::hash<int>{}(c.age());
+			std::size_t h2 = std::hash<string>{}(c.name());
+			return h1 ^ h2;
+		}
+	};
+}
 int main()
 {
 	unordered_set<string> uSet;
@@ -27,4 +78,26 @@ int main()
 	cout << "Hash 값 : " << hash<string>{}("d") << uSet.bucket("d") << endl;
 	
 	cout << "bucket count : " << uSet.bucket_count() << endl;
+
+
+	Cat kitty{ 1, "kitty" };
+	Cat nabi{ 1, "nabi" };
+
+	// error, hash 값과 equal의 정의가 되어있지않다.
+	// custom hash와 == 연산자를 정의해준다.
+	unordered_set<Cat> cats;
+	cats.emplace(kitty);
+	cats.emplace(nabi); 
+	
+	// error X
+	unordered_set<Cat, CatHash> cats;
+	cats.emplace(kitty);
+	cats.emplace(nabi);
+
+	// with namespace injection
+	unordered_set<Cat> cats;
+	cats.emplace(kitty);
+	cats.emplace(nabi);
+	
+	// multiset 이라면 중복 허용
 }
